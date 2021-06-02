a09d483a728224ea23b3b9ed9e9489217d034ab4
MiriErnfeld <miri@leader.codes>
2021-06-02 10:41:15 +0300

    --- Auto-Git Commit ---

--- src/redux/middleWares/midlleWare.js
+++ src/redux/middleWares/midlleWare.js
@@ -24,39 +24,39 @@ import actions from '../actions/actions'
 
 export const getDataFromSwapi = ({ getState, dispatch }) => (next) => (action) => {
 
-    if (action.type == "SET_CURRENT_DATA") {
-
-        let id = action.payLoad
-
-        axios.get(`https://swapi.dev/api/people/${id}/`).then(response => {
-            console.log(response)
-            return response.data
-        }).catch(o => {
-            console.log(o);
-        });
-    }
     // if (action.type == "SET_CURRENT_DATA") {
-    //     debugger
-    //     let id = action.payLoad
-    //     var myHeaders = new Headers();
-    //     myHeaders.append("Cookie", "cf_ob_info=502:653dc5431dd14c13:AMS; cf_use_ob=0");
-    //     var requestOptions = {
-    //         method: 'GET',
-    //         headers: myHeaders
-    //     };
 
-    //     fetch(`https://swapi.dev/api/people/${id}/`, requestOptions)
-    //         .then(response => response.json())
-    //         .then(result => {
-    //             debugger
-    //             console.log(result);
-    //             dispatch(actions.setId(id));
-    //             dispatch(actions.setNameFromSwapi(result));
-    //             debugger
-    //             dispatch(actions.setHairColorFromSwapi(result));
-    //         })
-    //         .catch(error => console.log('error', error));
-    //     return next(action)
+    //     let id = action.payLoad
+
+    //     axios.get(`https://swapi.dev/api/people/${id}/`).then(response => {
+    //         console.log(response)
+    //         return response.data
+    //     }).catch(o => {
+    //         console.log(o);
+    //     });
     // }
+    if (action.type == "SET_CURRENT_DATA") {
+        debugger
+        let id = action.payLoad
+        var myHeaders = new Headers();
+        myHeaders.append("Cookie", "cf_ob_info=502:653dc5431dd14c13:AMS; cf_use_ob=0");
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders
+        };
+
+        fetch(`https://swapi.dev/api/people/${id}/`, requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                debugger
+                console.log(result);
+                dispatch(actions.setId(id));
+                dispatch(actions.setNameFromSwapi(result));
+                debugger
+                dispatch(actions.setHairColorFromSwapi(result));
+            })
+            .catch(error => console.log('error', error));
+        return next(action)
+    }
     return next(action)
 }
